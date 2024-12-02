735f6627d37082f52ad6f5daa19b50c3e734a8f1
mxs2301 <90660823+mxs2301@users.noreply.github.com>
2024-12-02 14:13:43 +0100

    Create main.rs

---
+++ src/bin/day_2/main.rs
@@ -0,0 +1,8 @@
+mod functions_day2;
+use functions_day2::get_ansear_riddle_1;
+use functions_day2::get_answear_riddle_2;
+fn main() {
+
+    println!("Answear riddle 1: {}", get_ansear_riddle_1());
+    println!("Answear riddle 2: {}", get_answear_riddle_2())
+}
